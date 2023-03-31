dbaa8979341f361e9337991e37bdb610aa47b410
sophia altaf <Sophia@sophias-mbp.home>
2023-03-31 15:53:45 +0100

    initial App.js file coded

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Page from "./components/Page";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import contact from "./components/Contact";
+import PageContent from "./components/PageContent";
+import Portfolio from "./components/Portfolio";
+import Project from "./components/Project";
+import Resume from "./components/Resume";
+
+function App() {
+  const [pages] = useState([
+    {
+      name: "about me"
+    },
+    { name: "portfolio" },
+    { name: "contact" },
+    {
+      name: "resume"
+    }
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
+  return (
+    <div>
+      <Header>
+        <Nav
+          pages={pages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        ></Nav>
+      </Header>
+      <main>
+        <Page currentPage={currentPage}></Page>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
