58200efbfaa9f3172f7389087aadde43f803c5cf
sophia altaf <Sophia@sophias-mbp.home>
2023-04-01 13:28:32 +0100

    index.js file structure

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,22 +1,97 @@
 import React from 'react';
 import profileimage from "../../assets/Large/Profile/Profile_Pic.png";
-function About() {
+import { motion } from "framer-motion";
+import SocialIcons from "../components/SocialIcons";
+import { useInView } from "react-intersection-observer";
+import { useState, useEffect } from "react";
+import resume from "../pages/about/Sophia-Altaf-resume.pdf";
+
+const About = ({ name, email, location, availability, brand }) => {
+  const [ref, inView] = useInView({
+    threshold: 0.2,
+    triggerOnce: true,
+  });
+
+  const [downloading, setDownloading] = useState(false);
+
+  useEffect(() => {
+    setDownloading(false);
+  }, [downloading]);
+
+  const handleDownload = () => {
+    setDownloading(true);
+    const link = document.createElement("a");
+    link.href = resume;
+    link.download = "Sophia-Altaf-Resume.pdf";
+    link.onload = () => {
+      link.remove();
+      setDownloading(false);
+    };
+    document.body.appendChild(link);
+    link.click();
+  };
+
   return (
-    <section className="my-5">
-      <div className="my-2">
-        <div className="profile-img my-5">
-        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
-        </div>
-        <p>
-        Hello!
-        My name is Sophia Altaf.  I am currently studying Full Stack Web Developing at the University of Birmingham, UK.  I have a background in financial sales and insurance.  
-        </p>
-        <p>
-        I live in a village in the West Midlands, with my partner and two cats.
-        </p>
+    <div className="aboutContainer container">
+      <div className="row">
+        <motion.div
+          className="personalImage col-12 col-lg-4"
+          ref={ref}
+          initial={{ x: "-10vw", opacity: 0 }}
+          animate={inView ? { x: 0, opacity: 1 } : { x: "-10vw", opacity: 0 }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
+        >
+          <img src={aboutImg} alt={name} />
+        </motion.div>
+        <motion.div
+          className="personalInfo col-12 col-lg-8"
+          ref={ref}
+          initial={{ x: "10vw", opacity: 0 }}
+          animate={inView ? { x: 0, opacity: 1 } : { x: "10vw", opacity: 0 }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
+        >
+          <div className="contentContainer">
+            <h4>Nice to meet you</h4>
+            <h5>Student Web Developer creating one app at a time!</h5>
+            <div className="contentDescription">
+              <p>{brand}</p>
+            </div>
+            <div className="infoContainer">
+              <div className="row">
+                <div className="col-12 col-md-6 info">
+                  <span>Name:</span>
+                  <p>Sophia Altaf</p>
+                </div>
+                <div className="col-12 col-md-6 info">
+                  <span>Email:</span>
+                  <p>
+                    <a href={`mailto:${email}`}>{email}</a>
+                  </p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12 col-md-6 info">
+                  <span>Location:</span>
+                  <p>{location}</p>
+                </div>
+                <div className="col-12 col-md-6 info">
+                  <span>Availability:</span>
+                  <p>{availability}</p>
+                </div>
+              </div>
+            </div>
+            <div className="buttonContainer">
+              <button className="btn downloadCV" onClick={handleDownload} disabled={downloading}>
+                {downloading ? "Downloading..." : "Download Resume"}
+              </button>{" "}
+              <SocialIcons />
+            </div>
+          </div>
+        </motion.div>
       </div>
-    </section>
+    </div>
   );
-}
+};
+
 
 export default About;
