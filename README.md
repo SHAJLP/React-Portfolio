059e06842440a7b0cae6bfe6d4877d6cd209e438
sophia altaf <Sophia@sophias-mbp.home>
2023-04-06 09:27:27 +0100

    changes to files

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # React-Portfolio
+
+Description
+___________________________________________________________________
+A simple portfolio that includes contact details, links to projects and a little about myself.  I shall continue adding projects, modifying content and update the look and fuctionality.  Built with React.
+
+Features
+___________________________________________________________________
+
+* Responsive site
+* Contact form
+* Links to social media
+* links to Github profile and live demo's
+
+Technologies
+___________________________________________________________________
+
+* React.js
+* Netlify
+* web3forms
+* CSS
+
+License
+__________________________________________________________________________________________________________________________________
+
+This project is license under MIT ![Github licence](http://img.shields.io/badge/license-MIT-blue.svg)
+
+
+Images
+___________________________________________________________________
+
--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-import React, { useState } from "react";
 import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Page from "./components/Page";
-import Footer from "./components/Footer";
+import AnimatedRoutes from "./components/AnimatedRoutes";
 
 function App() {
-  const [pages] = useState([
-    {
-      name: "about me"
-    },
-    { name: "portfolio" },
-    { name: "contact" },
-    {
-      name: "resume"
-    }
-  ]);
-
-  const [currentPage, setCurrentPage] = useState(pages[0]);
+  const personalDetails = {
+    name: "Sophia Altaf",
+    location: "West Midlands, UK",
+    email: "sophiaaltaf73@gmail.com",
+    availability: "Open for work experience",
+    brand:
+      "Hello! I'm Sophia. Im currently a a student developer through the University of Birmingham, UK.  Before I started this course, I barely used my expensive laptop now its heaving under all of my downloads, installations and files!  I am looking forward to exploring roles in this field in the coming months and challenge myself further.  ",
+  };
 
   return (
-    <div>
-      <Header>
-        <Nav
-          pages={pages}
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage}
-        ></Nav>
-      </Header>
-      <main>
-        <Page currentPage={currentPage}></Page>
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Header />
+      <AnimatedRoutes personalDetails={personalDetails} />
+    </>
   );
 }
 
