70e3575a98df2dfe5e9f9ba8a8648e17ac8d3a5c
sophia altaf <Sophia@sophias-mbp.home>
2023-04-06 12:06:49 +0100

    file changes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import SocialIcons from "../components/SocialIcons";
 import { useInView } from "react-intersection-observer";
 import { useState, useEffect } from "react";
-import resume from "../pages/about/Sophia-Altaf-resume.pdf";
+//import resume from "../pages/about/Sophia-Altaf-resume.pdf";
 
 const AboutMe = ({ name, email, location, availability, brand,resume }) => {
   const [ref, inView] = useInView({
